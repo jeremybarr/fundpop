d27eda1b292e1b31eea24501e4fc5e38db76a395
Jeremy Barr <jeremy.barr@dominos.com>
2016-06-17 21:56:19 -0400

    initial commit

---
+++ fundpop/fundpop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fundpop
+//
+//  Created by Jeremy Barr (IS - Emg Tech) on 6/17/16.
+//  Copyright © 2016 FundPOp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ fundpop/fundpop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fundpop
+//
+//  Created by Jeremy Barr (IS - Emg Tech) on 6/17/16.
+//  Copyright © 2016 FundPOp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
